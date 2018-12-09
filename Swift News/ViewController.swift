ecdb06ace42dc63bb5f32c6a9e833f6a7803d4c7
Marc Aupont <mdaupont@gmail.com>
2018-12-09 17:38:51 -0500

    Initial Commit

---
+++ Swift News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift News
+//
+//  Created by Marc Aupont on 12/9/18.
+//  Copyright © 2018 Marc Aupont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
